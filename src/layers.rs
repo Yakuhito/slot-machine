a494853baface38a351618b383b168a550855dca
Yak <y@kuhi.to>
2024-08-28 18:40:57 +0300

    ph tests

--- src/layers.rs
+++ src/layers.rs
@@ -1,3 +1,26 @@
 mod state_layer;
 
 pub use state_layer::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chia::clvm_utils::tree_hash;
+
+    // we really have to expose this in chia-sdk-test
+    macro_rules! assert_puzzle_hash {
+        ($puzzle:ident => $puzzle_hash:ident) => {
+            let mut a = clvmr::Allocator::new();
+            let ptr = clvmr::serde::node_from_bytes(&mut a, &$puzzle)?;
+            let hash = tree_hash(&mut a, ptr);
+            assert_eq!($puzzle_hash, hash);
+        };
+    }
+
+    #[test]
+    fn test_puzzle_hashes() -> anyhow::Result<()> {
+        assert_puzzle_hash!(ACTION_LAYER_PUZZLE => ACTION_LAYER_PUZZLE_HASH);
+
+        Ok(())
+    }
+}
