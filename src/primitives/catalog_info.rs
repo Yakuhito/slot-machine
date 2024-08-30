526767037bbc588b2ad89122f252f87069318822
Yak <y@kuhi.to>
2024-08-30 12:48:52 +0300

    add constants

--- src/primitives/catalog_info.rs
+++ src/primitives/catalog_info.rs
@@ -15,37 +15,29 @@ pub struct CatalogState {
 
 #[must_use]
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct CatalogInfo {
-    pub launcher_id: Bytes32,
+pub struct CatalogConstants {
     pub royalty_address_hash: Bytes32,
     pub trade_price_percentage: u8,
     pub precommit_payout_puzzle_hash: Bytes32,
     pub relative_block_height: u32,
     pub price_singleton_launcher_id: Bytes32,
+}
 
+#[must_use]
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct CatalogInfo {
+    pub launcher_id: Bytes32,
     pub state: CatalogState,
+
+    pub constants: CatalogConstants,
 }
 
 impl CatalogInfo {
-    pub fn new(
-        launcher_id: Bytes32,
-        royalty_address_hash: Bytes32,
-        trade_price_percentage: u8,
-        precommit_payout_puzzle_hash: Bytes32,
-        relative_block_height: u32,
-        price_singleton_launcher_id: Bytes32,
-        state: CatalogState,
-    ) -> Self {
+    pub fn new(launcher_id: Bytes32, state: CatalogState, constants: CatalogConstants) -> Self {
         Self {
             launcher_id,
-            royalty_address_hash,
-            trade_price_percentage,
-            precommit_payout_puzzle_hash,
-            relative_block_height,
-            price_singleton_launcher_id,
             state,
+            constants,
         }
     }
 }
-
-// todo: config to initialize this more easily + impl
