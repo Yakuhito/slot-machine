bc2cc2056ad8ab5e984bb4d0e1b995b50dc3afdb
Yak <y@kuhi.to>
2024-09-06 11:47:28 -0300

    parse_one_sided_offer

--- src/drivers.rs
+++ src/drivers.rs
@@ -1,6 +1,132 @@
-use chia::protocol::SpendBundle;
-use chia_wallet_sdk::{DriverError, Offer};
+use bip39::Mnemonic;
+use chia::{
+    bls::{SecretKey, Signature},
+    protocol::{Bytes32, Coin, CoinSpend, SpendBundle},
+    puzzles::{
+        offer::{
+            NotarizedPayment, Payment, SettlementPaymentsSolution, SETTLEMENT_PAYMENTS_PUZZLE_HASH,
+        },
+        standard::StandardArgs,
+    },
+};
+use chia_wallet_sdk::{Condition, DriverError, Offer, SpendContext};
+use clvm_traits::{FromClvm, ToClvm};
+use clvmr::NodePtr;
 
-pub fn launch_catalog(offer: Offer) -> Result<SpendBundle, DriverError> {
+pub struct SecureOneSidedOffer {
+    pub coin_spends: Vec<CoinSpend>,
+    pub aggregated_signature: Signature,
+    pub security_coin: Coin,
+    pub security_coin_sk: SecretKey,
+}
+
+fn custom_err<T>(e: T) -> DriverError
+where
+    T: ToString,
+{
+    DriverError::Custom(e.to_string())
+}
+
+pub fn parse_one_sided_offer(
+    ctx: &mut SpendContext,
+    offer: Offer,
+) -> Result<SecureOneSidedOffer, DriverError> {
+    let offer = offer.parse(&mut ctx.allocator).map_err(custom_err)?;
+
+    if !offer.requested_payments.is_empty() {
+        return Err(DriverError::Custom(
+            "Launch offer should not have any requested payments".to_string(),
+        ));
+    }
+
+    // we need the security coin puzzle hash to spend the offer coin after finding it
+    let mut entropy = [0u8; 32];
+    getrandom::getrandom(&mut entropy).map_err(custom_err)?;
+    let mnemonic = Mnemonic::from_entropy(&entropy).map_err(custom_err)?;
+    let seed = mnemonic.to_seed("");
+    let sk = SecretKey::from_seed(&seed);
+    let security_coin_puzzle_hash: Bytes32 = StandardArgs::curry_tree_hash(sk.public_key()).into();
+
+    // returned spends will also spend the offer coin (creating the security coin)
+    let mut coin_spends = Vec::with_capacity(offer.coin_spends.len() + 1);
+    let mut security_coin_parent_id: Option<Bytes32> = None;
+    let mut security_coin_amount = 0;
+
+    for coin_spend in offer.coin_spends {
+        if security_coin_parent_id.is_none() {
+            let puzzle_ptr = coin_spend.puzzle_reveal.to_clvm(&mut ctx.allocator)?;
+            let solution_ptr = coin_spend.solution.to_clvm(&mut ctx.allocator)?;
+
+            let res = ctx.run(puzzle_ptr, solution_ptr)?;
+            let res = Vec::<Condition<NodePtr>>::from_clvm(&ctx.allocator, res)?;
+
+            if let Some(cc) = res
+                .into_iter()
+                .filter_map(|cond| {
+                    let Condition::CreateCoin(cc) = cond else {
+                        return None;
+                    };
+
+                    Some(cc)
+                })
+                .find(|cc| cc.puzzle_hash == SETTLEMENT_PAYMENTS_PUZZLE_HASH.into())
+            {
+                let offer_coin = Coin::new(
+                    coin_spend.coin.coin_id(),
+                    SETTLEMENT_PAYMENTS_PUZZLE_HASH.into(),
+                    cc.amount,
+                );
+                let offer_coin_id = offer_coin.coin_id();
+
+                security_coin_parent_id = Some(offer_coin_id);
+                security_coin_amount = cc.amount;
+
+                let offer_coin_puzzle = ctx.settlement_payments_puzzle()?;
+                let offer_coin_puzzle = ctx.serialize(&offer_coin_puzzle)?;
+
+                let offer_coin_solution = SettlementPaymentsSolution {
+                    notarized_payments: vec![NotarizedPayment {
+                        nonce: offer_coin_id,
+                        payments: vec![Payment {
+                            puzzle_hash: security_coin_puzzle_hash,
+                            amount: security_coin_amount,
+                            memos: vec![],
+                        }],
+                    }],
+                };
+                let offer_coin_solution = ctx.serialize(&offer_coin_solution)?;
+
+                let offer_coin_spend =
+                    CoinSpend::new(offer_coin, offer_coin_puzzle, offer_coin_solution);
+                coin_spends.push(offer_coin_spend);
+            }
+        }
+
+        coin_spends.push(coin_spend);
+    }
+
+    let Some(security_coin_parent_id) = security_coin_parent_id else {
+        return Err(DriverError::Custom(
+            "Launch offer should offer XCH".to_string(),
+        ));
+    };
+
+    let security_coin = Coin::new(
+        security_coin_parent_id,
+        security_coin_puzzle_hash,
+        security_coin_amount,
+    );
+
+    Ok(SecureOneSidedOffer {
+        coin_spends,
+        aggregated_signature: offer.aggregated_signature,
+        security_coin,
+        security_coin_sk: sk,
+    })
+}
+
+pub fn launch_catalog(ctx: &mut SpendContext, offer: Offer) -> Result<SpendBundle, DriverError> {
+    let offer = parse_one_sided_offer(ctx, offer)?;
+
     todo!()
 }
