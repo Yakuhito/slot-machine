f0e509da62082860922b6a1e3fac61798f23ba94
Yak <y@kuhi.to>
2024-09-07 09:56:17 -0300

    secure preroller lol

--- src/layers.rs
+++ src/layers.rs
@@ -1,9 +1,11 @@
 mod action_layer;
 mod actions;
+mod catalog_preroller;
 mod conditions_layer;
 mod state_scheduler_layer;
 
 pub use action_layer::*;
 pub use actions::*;
+pub use catalog_preroller::*;
 pub use conditions_layer::*;
 pub use state_scheduler_layer::*;
