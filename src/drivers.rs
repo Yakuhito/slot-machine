500fb9783e2283ca1bd67ffaa0ba5521497772df
Yak <y@kuhi.to>
2024-09-08 15:04:01 -0300

    rm debug

--- src/drivers.rs
+++ src/drivers.rs
@@ -350,10 +350,7 @@ mod tests {
     use chia_wallet_sdk::{test_secret_keys, Simulator, SpendWithConditions, TESTNET11_CONSTANTS};
     use hex_literal::hex;
 
-    use crate::{
-        print_spend_bundle_to_file, AddCatInfo, CatNftMetadata, CatalogPrecommitValue,
-        PrecommitCoin,
-    };
+    use crate::{AddCatInfo, CatNftMetadata, CatalogPrecommitValue, PrecommitCoin};
 
     use super::*;
 
@@ -543,9 +540,7 @@ mod tests {
         let solution_program = ctx.serialize(&NodePtr::NIL)?;
         ctx.insert(CoinSpend::new(funds_coin, funds_program, solution_program));
 
-        let spends = ctx.take();
-        print_spend_bundle_to_file(spends.clone(), Signature::default(), "sb.debug");
-        sim.spend_coins(spends, &[user_sk])?;
+        sim.spend_coins(ctx.take(), &[user_sk])?;
 
         Ok(())
     }
