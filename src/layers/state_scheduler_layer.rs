b2f9df558c20ad170317249dff3b8673412f16aa
Yak <y@kuhi.to>
2024-09-09 15:30:30 -0300

    update state scheduler

--- src/layers/state_scheduler_layer.rs
+++ src/layers/state_scheduler_layer.rs
@@ -5,7 +5,7 @@ use chia::{
         SingletonStruct, SINGLETON_LAUNCHER_PUZZLE_HASH, SINGLETON_TOP_LAYER_PUZZLE_HASH,
     },
 };
-use chia_wallet_sdk::{DriverError, Layer, Puzzle, SpendContext};
+use chia_wallet_sdk::{Condition, Conditions, DriverError, Layer, Puzzle, SpendContext};
 use clvm_traits::{FromClvm, ToClvm};
 use clvmr::{Allocator, NodePtr};
 use hex_literal::hex;
@@ -48,7 +48,7 @@ impl Layer for StateSchedulerLayer {
             return Ok(None);
         }
 
-        let args = StateSchedulerLayerArgs::from_clvm(allocator, puzzle.args)?;
+        let args = StateSchedulerLayerArgs::<NodePtr>::from_clvm(allocator, puzzle.args)?;
 
         if args.other_singleton_struct.launcher_puzzle_hash != SINGLETON_LAUNCHER_PUZZLE_HASH.into()
             || args.other_singleton_struct.mod_hash != SINGLETON_TOP_LAYER_PUZZLE_HASH.into()
@@ -56,11 +56,33 @@ impl Layer for StateSchedulerLayer {
             return Err(DriverError::NonStandardLayer);
         }
 
+        let conditions = Conditions::<NodePtr>::from_clvm(allocator, args.base_conditions)?;
+        let (
+            Some(Condition::AssertHeightAbsolute(assert_height_condition)),
+            Some(Condition::CreateCoin(create_coin_condition)),
+        ) = conditions
+            .into_iter()
+            .fold(
+                (None, None),
+                |(assert_height, create_coin), cond| match cond {
+                    Condition::AssertHeightAbsolute(_) if assert_height.is_none() => {
+                        (Some(cond), create_coin)
+                    }
+                    Condition::CreateCoin(_) if create_coin.is_none() => {
+                        (assert_height, Some(cond))
+                    }
+                    _ => (assert_height, create_coin),
+                },
+            )
+        else {
+            return Err(DriverError::NonStandardLayer);
+        };
+
         Ok(Some(Self {
             other_singleton_launcher_id: args.other_singleton_struct.launcher_id,
             new_state_hash: args.new_state_hash,
-            required_block_height: args.required_block_height,
-            new_puzzle_hash: args.new_puzzle_hash,
+            required_block_height: assert_height_condition.height,
+            new_puzzle_hash: create_coin_condition.puzzle_hash,
         }))
     }
 
@@ -72,13 +94,16 @@ impl Layer for StateSchedulerLayer {
     }
 
     fn construct_puzzle(&self, ctx: &mut SpendContext) -> Result<NodePtr, DriverError> {
+        let base_conditions = Conditions::new()
+            .create_coin(self.new_puzzle_hash, 1, vec![])
+            .assert_height_absolute(self.required_block_height);
+
         CurriedProgram {
             program: ctx.state_shcheduler_puzzle()?,
             args: StateSchedulerLayerArgs {
                 other_singleton_struct: SingletonStruct::new(self.other_singleton_launcher_id),
                 new_state_hash: self.new_state_hash,
-                required_block_height: self.required_block_height,
-                new_puzzle_hash: self.new_puzzle_hash,
+                base_conditions,
             },
         }
         .to_clvm(&mut ctx.allocator)
@@ -94,37 +119,37 @@ impl Layer for StateSchedulerLayer {
     }
 }
 
-pub const STATE_SCHEDULER_PUZZLE: [u8; 503] = hex!("ff02ffff01ff04ffff04ff18ffff04ff2fffff01ff01808080ffff04ffff04ff10ffff04ff17ff808080ffff04ffff04ff14ffff04ffff0112ffff04ff0bffff04ffff02ff16ffff04ff02ffff04ff09ffff04ffff02ff3effff04ff02ffff04ff05ff80808080ffff04ff5fff808080808080ff8080808080ff80808080ffff04ffff01ffffff5333ff4202ffffff02ffff03ff05ffff01ff0bff7affff02ff2effff04ff02ffff04ff09ffff04ffff02ff12ffff04ff02ffff04ff0dff80808080ff808080808080ffff016a80ff0180ffffa04bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c385a5d7cce23c7785459aa09dcf97a184f32623d11a73124ceb99a5709b083721e878a16d78f596718ba7b2ffa102a12871fee210fb8619291eaea194581cbd2531e4b23759d225f6806923f63222a102a8d5dd63fba471ebcb1f3e8f7c1e1879b7152a6e7298a91ce119a63400ade7c5ffff0bff5affff02ff2effff04ff02ffff04ff05ffff04ffff02ff12ffff04ff02ffff04ff07ff80808080ff808080808080ffff0bff1cffff0bff1cff6aff0580ffff0bff1cff0bff4a8080ff02ffff03ffff07ff0580ffff01ff0bffff0102ffff02ff3effff04ff02ffff04ff09ff80808080ffff02ff3effff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ff018080");
+pub const STATE_SCHEDULER_PUZZLE: [u8; 419] = hex!("ff02ffff01ff04ffff04ff14ffff04ff2fffff01ff01808080ffff04ffff04ff08ffff04ff17ff808080ffff04ffff04ff1cffff04ffff0112ffff04ff0bffff04ffff0bff56ffff0bff0affff0bff0aff66ff0980ffff0bff0affff0bff76ffff0bff0affff0bff0aff66ffff02ff1effff04ff02ffff04ff05ff8080808080ffff0bff0affff0bff76ffff0bff0affff0bff0aff66ff5f80ffff0bff0aff66ff46808080ff46808080ff46808080ff8080808080ff80808080ffff04ffff01ffff53ff3342ff02ffffffa04bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c385a5d7cce23c7785459aa09dcf97a184f32623d11a73124ceb99a5709b083721e878a16d78f596718ba7b2ffa102a12871fee210fb8619291eaea194581cbd2531e4b23759d225f6806923f63222a102a8d5dd63fba471ebcb1f3e8f7c1e1879b7152a6e7298a91ce119a63400ade7c5ff02ffff03ffff07ff0580ffff01ff0bffff0102ffff02ff1effff04ff02ffff04ff09ff80808080ffff02ff1effff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ff018080");
 
 pub const STATE_SCHEDULER_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    f081173cc82c6940a0c0a9f35b7ae5e75ff7befa431ac97f216af94328b9a8be
+    830d9435f772768e9c05a42c54a000b359b7ce239eda46dc95c7bf674f531e37
     "
 ));
 
-#[derive(FromClvm, ToClvm, Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(FromClvm, ToClvm, Debug, Clone, PartialEq, Eq)]
 #[clvm(list)]
-pub struct StateSchedulerLayerArgs {
+pub struct StateSchedulerLayerArgs<T> {
     pub other_singleton_struct: SingletonStruct,
+    pub base_conditions: T,
     pub new_state_hash: Bytes32,
-    pub required_block_height: u32,
-    pub new_puzzle_hash: Bytes32,
 }
 
-impl StateSchedulerLayerArgs {
+impl<T> StateSchedulerLayerArgs<T>
+where
+    T: ToTreeHash,
+{
     pub fn curry_tree_hash(
         other_singleton_launcher_id: Bytes32,
+        base_conditions: T,
         new_state_hash: Bytes32,
-        required_block_height: u32,
-        new_puzzle_hash: Bytes32,
     ) -> TreeHash {
         CurriedProgram {
             program: STATE_SCHEDULER_PUZZLE_HASH,
             args: StateSchedulerLayerArgs {
                 other_singleton_struct: SingletonStruct::new(other_singleton_launcher_id),
+                base_conditions: base_conditions.tree_hash(),
                 new_state_hash,
-                required_block_height,
-                new_puzzle_hash,
             },
         }
         .tree_hash()
