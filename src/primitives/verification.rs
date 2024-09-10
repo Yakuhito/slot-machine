1b49c755e9f7208e25920e7af474b7e744d26eda
Yak <y@kuhi.to>
2024-09-10 14:20:46 -0300

    Verification primitive

--- src/primitives/verification.rs
+++ src/primitives/verification.rs
@@ -1,123 +1,118 @@
 use chia::{
-    clvm_utils::{CurriedProgram, ToTreeHash, TreeHash},
+    clvm_utils::{ToTreeHash, TreeHash},
     protocol::{Bytes32, Coin, CoinSpend},
-    puzzles::singleton::SingletonStruct,
+    puzzles::{
+        singleton::{SingletonArgs, SingletonSolution},
+        EveProof, Proof,
+    },
 };
-use chia_wallet_sdk::{DriverError, SpendContext};
-use clvm_traits::{FromClvm, ToClvm};
+use chia_wallet_sdk::{DriverError, Layer, SingletonLayer, SpendContext};
 use clvmr::NodePtr;
-use hex_literal::hex;
 
-use crate::SpendContextExt;
+use crate::{VerificationLayer, VerificationLayer2ndCurryArgs, VerificationLayerSolution};
+
+use super::VerificationInfo;
+
+type VerificationLayers = SingletonLayer<VerificationLayer>;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[must_use]
-pub struct Slot<V>
-where
-    V: Copy,
-{
+pub struct Verification {
     pub coin: Coin,
-    pub proof: SlotProof,
+    pub proof: Proof,
 
-    pub info: SlotInfo<V>,
+    pub info: VerificationInfo,
 }
 
-impl<V> Slot<V>
-where
-    V: Copy,
-{
-    pub fn new(proof: SlotProof, info: SlotInfo<V>) -> Self {
-        let parent_coin_id = proof.slot_parent_id(info.launcher_id);
+impl Verification {
+    pub fn new(coin: Coin, proof: Proof, info: VerificationInfo) -> Self {
+        Self { coin, proof, info }
+    }
 
+    pub fn after_mint(launcher_parent: Bytes32, info: VerificationInfo) -> Self {
         Self {
-            coin: Coin::new(parent_coin_id, Slot::<V>::puzzle_hash(&info).into(), 0),
-            proof,
+            coin: Coin::new(info.launcher_id, Self::puzzle_hash(&info).into(), 1),
+            proof: Proof::Eve(EveProof {
+                parent_parent_coin_info: launcher_parent,
+                parent_amount: 0,
+            }),
             info,
         }
     }
 
-    pub fn first_curry_hash(launcher_id: Bytes32) -> TreeHash {
-        CurriedProgram {
-            program: SLOT_PUZZLE_HASH,
-            args: Slot1stCurryArgs {
-                singleton_struct: SingletonStruct::new(launcher_id),
-            },
-        }
-        .tree_hash()
+    pub fn inner_puzzle_hash<T>(
+        revocation_singleton_launcher_id: Bytes32,
+        verified_data: T,
+    ) -> TreeHash
+    where
+        T: ToTreeHash,
+    {
+        VerificationLayer2ndCurryArgs::curry_tree_hash(
+            revocation_singleton_launcher_id,
+            verified_data,
+        )
     }
 
-    pub fn puzzle_hash(info: &SlotInfo<V>) -> TreeHash {
-        CurriedProgram {
-            program: Self::first_curry_hash(info.launcher_id),
-            args: Slot2ndCurryArgs {
-                value_hash: info.value_hash,
-            },
-        }
-        .tree_hash()
+    pub fn puzzle_hash(info: &VerificationInfo) -> TreeHash {
+        SingletonArgs::curry_tree_hash(
+            info.launcher_id,
+            Self::inner_puzzle_hash(
+                info.revocation_singleton_launcher_id,
+                info.verified_data.tree_hash(),
+            ),
+        )
+    }
+
+    pub fn into_layers(self) -> VerificationLayers {
+        SingletonLayer::new(
+            self.info.launcher_id,
+            VerificationLayer::new(
+                self.info.revocation_singleton_launcher_id,
+                self.info.verified_data,
+            ),
+        )
+    }
+
+    pub fn into_layers_with_clone(&self) -> VerificationLayers {
+        SingletonLayer::new(
+            self.info.launcher_id,
+            VerificationLayer::new(
+                self.info.revocation_singleton_launcher_id,
+                self.info.verified_data.clone(),
+            ),
+        )
     }
 
     pub fn construct_puzzle(&self, ctx: &mut SpendContext) -> Result<NodePtr, DriverError> {
-        let prog_1st_curry = CurriedProgram {
-            program: ctx.slot_puzzle()?,
-            args: Slot1stCurryArgs {
-                singleton_struct: SingletonStruct::new(self.info.launcher_id),
-            },
-        }
-        .to_clvm(&mut ctx.allocator)?;
+        let layers = self.into_layers_with_clone();
 
-        Ok(CurriedProgram {
-            program: prog_1st_curry,
-            args: Slot2ndCurryArgs {
-                value_hash: self.info.value_hash,
-            },
-        }
-        .to_clvm(&mut ctx.allocator)?)
+        layers.construct_puzzle(ctx)
     }
 
     pub fn spend(
         self,
         ctx: &mut SpendContext,
-        spender_inner_puzzle_hash: Bytes32,
+        revocation_singleton_inner_puzzle_hash: Option<Bytes32>,
     ) -> Result<(), DriverError> {
-        let puzzle_reveal = self.construct_puzzle(ctx)?;
+        let sol = SingletonSolution {
+            lineage_proof: self.proof,
+            amount: self.coin.amount,
+            inner_solution: VerificationLayerSolution {
+                revocation_singleton_inner_puzzle_hash,
+            },
+        };
+        let my_coin = self.coin;
+
+        let layers = self.into_layers();
+
+        let puzzle_reveal = layers.construct_puzzle(ctx)?;
+        let solution = layers.construct_solution(ctx, sol)?;
+
         let puzzle_reveal = ctx.serialize(&puzzle_reveal)?;
+        let solution = ctx.serialize(&solution)?;
 
-        let solution = ctx.serialize(&SlotSolution {
-            parent_parent_info: self.proof.parent_parent_info,
-            parent_inner_puzzle_hash: self.proof.parent_inner_puzzle_hash,
-            spender_inner_puzzle_hash,
-        })?;
-
-        ctx.insert(CoinSpend::new(self.coin, puzzle_reveal, solution));
+        ctx.insert(CoinSpend::new(my_coin, puzzle_reveal, solution));
 
         Ok(())
     }
-}
-
-pub const SLOT_PUZZLE: [u8; 539] = hex!("ff02ffff01ff04ffff04ff10ffff04ffff30ff17ffff02ff3effff04ff02ffff04ff05ffff04ff2fff8080808080ffff010180ff808080ffff04ffff04ff18ffff04ffff0112ffff04ff80ffff04ffff02ff3effff04ff02ffff04ff05ffff04ff5fff8080808080ff8080808080ff808080ffff04ffff01ffffff4743ff02ff02ffff03ff05ffff01ff0bff72ffff02ff16ffff04ff02ffff04ff09ffff04ffff02ff1cffff04ff02ffff04ff0dff80808080ff808080808080ffff016280ff0180ffffffffa04bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c385a5d7cce23c7785459aa09dcf97a184f32623d11a73124ceb99a5709b083721e878a16d78f596718ba7b2ffa102a12871fee210fb8619291eaea194581cbd2531e4b23759d225f6806923f63222a102a8d5dd63fba471ebcb1f3e8f7c1e1879b7152a6e7298a91ce119a63400ade7c5ff0bff52ffff02ff16ffff04ff02ffff04ff05ffff04ffff02ff1cffff04ff02ffff04ff07ff80808080ff808080808080ffff0bff14ffff0bff14ff62ff0580ffff0bff14ff0bff428080ffff02ffff03ffff07ff0580ffff01ff0bffff0102ffff02ff2effff04ff02ffff04ff09ff80808080ffff02ff2effff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ff02ff1affff04ff02ffff04ff09ffff04ffff02ff2effff04ff02ffff04ff05ff80808080ffff04ff0bff808080808080ff018080");
-
-pub const SLOT_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
-    "
-    54c2894144c2e41b7607e74f5a6c1acb5f53f15c8ebd8bea0beee81f3cce9f8b
-    "
-));
-
-#[derive(ToClvm, FromClvm, Debug, Clone, Copy, PartialEq, Eq)]
-#[clvm(curry)]
-pub struct Slot1stCurryArgs {
-    pub singleton_struct: SingletonStruct,
-}
-
-#[derive(ToClvm, FromClvm, Debug, Clone, Copy, PartialEq, Eq)]
-#[clvm(curry)]
-pub struct Slot2ndCurryArgs {
-    pub value_hash: Bytes32,
-}
-
-#[derive(ToClvm, FromClvm, Debug, Clone, Copy, PartialEq, Eq)]
-#[clvm(solution)]
-pub struct SlotSolution {
-    pub parent_parent_info: Bytes32,
-    pub parent_inner_puzzle_hash: Bytes32,
-    pub spender_inner_puzzle_hash: Bytes32,
 }
--- src/primitives/verification_info.rs
+++ src/primitives/verification_info.rs
@@ -1,93 +1,26 @@
-use std::cmp::Ordering;
+use chia::protocol::Bytes32;
 
-use chia::{clvm_utils::ToTreeHash, protocol::Bytes32};
-use clvm_traits::{FromClvm, ToClvm};
-use hex_literal::hex;
-use num_bigint::BigInt;
+use crate::VerifiedData;
 
-// the values below are for slots organized into a double-linked ordered list
-// the minimum possible value of an slot - this will be contained by one of the ends of the list
-pub static SLOT32_MIN_VALUE: [u8; 32] =
-    hex!("8000000000000000000000000000000000000000000000000000000000000000");
-// the maximum possible value of a slot - will be contained by the other end of the list
-pub static SLOT32_MAX_VALUE: [u8; 32] =
-    hex!("7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 #[must_use]
-pub struct SlotInfo<V>
-where
-    V: Copy,
-{
+pub struct VerificationInfo {
     pub launcher_id: Bytes32,
 
-    pub value_hash: Bytes32,
-    pub value: Option<V>,
+    pub revocation_singleton_launcher_id: Bytes32,
+    pub verified_data: VerifiedData,
 }
 
-impl<V> SlotInfo<V>
-where
-    V: Copy,
-{
-    pub fn new(launcher_id: Bytes32, value_hash: Bytes32) -> Self {
+impl VerificationInfo {
+    pub fn new(
+        launcher_id: Bytes32,
+        revocation_singleton_launcher_id: Bytes32,
+        verified_data: VerifiedData,
+    ) -> Self {
         Self {
             launcher_id,
-            value_hash,
-            value: None,
+            revocation_singleton_launcher_id,
+            verified_data,
         }
-    }
-
-    pub fn from_value(launcher_id: Bytes32, value: V) -> Self
-    where
-        V: ToTreeHash,
-    {
-        Self {
-            launcher_id,
-            value_hash: value.tree_hash().into(),
-            value: Some(value),
-        }
-    }
-}
-
-#[derive(ToClvm, FromClvm, Debug, Clone, Copy, PartialEq, Eq)]
-#[clvm(list)]
-pub struct CatalogSlotNeigborsInfo {
-    pub left_asset_id: Bytes32,
-    #[clvm(rest)]
-    pub right_asset_id: Bytes32,
-}
-
-#[derive(ToClvm, FromClvm, Debug, Clone, Copy, PartialEq, Eq)]
-#[clvm(list)]
-pub struct CatalogSlotValue {
-    pub asset_id: Bytes32,
-    #[clvm(rest)]
-    pub neighbors: CatalogSlotNeigborsInfo,
-}
-
-impl CatalogSlotValue {
-    pub fn new(asset_id: Bytes32, left_asset_id: Bytes32, right_asset_id: Bytes32) -> Self {
-        Self {
-            asset_id,
-            neighbors: CatalogSlotNeigborsInfo {
-                left_asset_id,
-                right_asset_id,
-            },
-        }
-    }
-}
-
-impl Ord for CatalogSlotValue {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let self_num = BigInt::from_signed_bytes_be(&self.asset_id);
-        let other_num = BigInt::from_signed_bytes_be(&other.asset_id);
-
-        self_num.cmp(&other_num)
-    }
-}
-
-impl PartialOrd for CatalogSlotValue {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
     }
 }
