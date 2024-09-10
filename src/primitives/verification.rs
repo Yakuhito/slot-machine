bcc3675c6d2b5abfce0ecb656365eea0d36037e5
Yak <y@kuhi.to>
2024-09-10 15:04:30 -0300

    tests: successfully launch verification coin

--- src/primitives/verification.rs
+++ src/primitives/verification.rs
@@ -116,3 +116,65 @@ impl Verification {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use anyhow::Ok;
+    use chia::{bls::Signature, puzzles::singleton::SINGLETON_LAUNCHER_PUZZLE_HASH};
+    use chia_wallet_sdk::{Conditions, Launcher, Simulator, StandardLayer};
+
+    use crate::{print_spend_bundle_to_file, VerifiedData};
+
+    use super::*;
+
+    #[test]
+    fn test_verifications() -> anyhow::Result<()> {
+        let mut sim = Simulator::new();
+        let ctx = &mut SpendContext::new();
+        let (sk, pk, _, coin) = sim.new_p2(2)?;
+        let p2 = StandardLayer::new(pk);
+
+        let did_launcher = Launcher::new(coin.coin_id(), 1);
+        let (create_did, did) = did_launcher.create_simple_did(ctx, &p2)?;
+        p2.spend(ctx, coin, create_did)?;
+
+        let did = did.update(
+            ctx,
+            &p2,
+            Conditions::new().create_coin(SINGLETON_LAUNCHER_PUZZLE_HASH.into(), 0, vec![]),
+        )?;
+        let verification_launcher = Launcher::new(did.coin.parent_coin_info, 0);
+
+        let test_info = VerificationInfo::new(
+            verification_launcher.coin().coin_id(),
+            did.coin.coin_id(),
+            VerifiedData {
+                version: 1,
+                asset_id: Bytes32::new([2; 32]),
+                data_hash: Bytes32::new([3; 32]),
+                category: "cat".to_string(),
+                subcategory: "subcat".to_string(),
+            },
+        );
+        let verification =
+            Verification::after_mint(verification_launcher.coin().parent_coin_info, test_info);
+
+        let (_conds, new_coin) = verification_launcher.with_singleton_amount(1).spend(
+            ctx,
+            Verification::inner_puzzle_hash(
+                verification.info.revocation_singleton_launcher_id,
+                verification.info.verified_data.clone(),
+            )
+            .into(),
+            (),
+        )?;
+
+        assert_eq!(new_coin, verification.coin);
+
+        let spends = ctx.take();
+        print_spend_bundle_to_file(spends.clone(), Signature::default(), "sb.debug");
+        sim.spend_coins(spends, &[sk])?;
+
+        Ok(())
+    }
+}
