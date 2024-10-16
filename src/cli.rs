4329df0826bd51d517f6db34300ec7a645bec8c4
Yak <y@kuhi.to>
2024-10-16 18:29:29 -0300

    rev to old file structure - yay!

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,13 @@
+mod catalog;
 mod chia_client;
 mod commands;
 mod csv;
 mod database;
-mod initiate_launch;
 mod utils;
 
+pub use catalog::*;
 pub use chia_client::*;
 pub use commands::*;
 pub use csv::*;
 pub use database::*;
-pub use initiate_launch::*;
 pub use utils::*;
