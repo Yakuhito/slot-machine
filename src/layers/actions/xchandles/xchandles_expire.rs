4544a961b8d9c0e49fe86513155cd14ba2611d97
Yak <y@kuhi.to>
2025-01-11 20:07:57 -0300

    exponential premium working

--- src/layers/actions/xchandles/xchandles_expire.rs
+++ src/layers/actions/xchandles/xchandles_expire.rs
@@ -112,12 +112,12 @@ pub struct XchandlesExpireActionSolution {
     pub launcher_id_hash: Bytes32,
 }
 
-pub const XCHANDLES_EXPONENTIAL_PREMIUM_RENEW_PUZZLE: [u8; 125] =
-    hex!("ff02ffff01ff04ffff04ffff11ff8202ffff82017f80ffff04ff02ffff04ffff0101ffff04ffff3dffff11ff8202ffff82017f80ff0280ffff04ffff14ffff11ff8202ffff82017f80ff0280ff808080808080ffff06ffff02ff05ffff04ff81bfffff04ff8205ffff808080808080ffff04ffff0183015180ff018080");
+pub const XCHANDLES_EXPONENTIAL_PREMIUM_RENEW_PUZZLE: [u8; 369] =
+    hex!("ff02ffff01ff04ffff10ffff05ffff02ff05ffff04ff81bfffff04ff8205ffff8080808080ffff02ff0effff04ff02ffff04ffff02ff0affff04ff02ffff04ff2fffff04ff5fffff04ffff0101ffff04ffff05ffff14ffff12ffff0183010000ffff06ffff14ffff11ff8202ffff82017f80ff04808080ff048080ffff04ffff05ffff14ff0bffff17ffff0101ffff05ffff14ffff11ff8202ffff82017f80ff048080808080ff8080808080808080ffff04ff17ff808080808080ffff06ffff02ff05ffff04ff81bfffff04ff8205ffff808080808080ffff04ffff01ff83015180ffff02ffff03ff0bffff01ff02ff0affff04ff02ffff04ff05ffff04ff1bffff04ffff17ff17ffff010180ffff04ff2fffff04ffff02ffff03ffff18ff2fff1780ffff01ff05ffff14ffff12ff5fff1380ff058080ffff015f80ff0180ff8080808080808080ffff015f80ff0180ff02ffff03ffff15ff05ff0b80ffff01ff11ff05ff0b80ff8080ff0180ff018080");
 
 pub const XCHANDLES_EXPONENTIAL_PREMIUM_RENEW_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    faa63cb8ac1800f95e0a4fa823e6755df7331c68d8f617b9adf5ea63fc889770
+    ae02a6a2992900bbb46a4216f38b74572b3efb71915932afbdcc827bd8576361
     "
 ));
 
@@ -207,14 +207,12 @@ pub struct XchandlesExponentialPremiumRenewPuzzleSolution<S> {
 
 #[cfg(test)]
 mod tests {
-    use clvmr::serde::node_to_bytes;
-
     use super::*;
 
     #[derive(FromClvm, ToClvm, Debug, Clone, PartialEq, Eq)]
     #[clvm(list)]
     pub struct XchandlesPricingOutput {
-        pub price: u64,
+        pub price: u128,
         #[clvm(rest)]
         pub registered_time: u64,
     }
@@ -226,12 +224,9 @@ mod tests {
         let puzzle =
             XchandlesExponentialPremiumRenewPuzzleArgs::from_scale_factor(&mut ctx, 0, 1000)?
                 .get_puzzle(&mut ctx)?;
-        println!(
-            "puzzle: {:?}",
-            hex::encode(node_to_bytes(&ctx.allocator, puzzle).unwrap())
-        );
 
-        for day in 0..2 {
+        let mut last_price = 100_000_000_000;
+        for day in 0..28 {
             for hour in 0..24 {
                 let solution = XchandlesExponentialPremiumRenewPuzzleSolution::<u64> {
                     handle: "yakuhito".to_string(),
@@ -242,24 +237,38 @@ mod tests {
                 .to_clvm(&mut ctx.allocator)?;
 
                 let output = ctx.run(puzzle, solution)?;
-                // let output = XchandlesPricingOutput::from_clvm(&ctx.allocator, output)?;
-                println!(
-                    "day:\t{}\thour:\t{}\tbuy_time:\t{}\t- result is\t{:}",
-                    day,
-                    hour,
-                    day * 24 * 60 * 60 + hour * 60 * 60,
-                    // output.price
-                    hex::encode(node_to_bytes(&ctx.allocator, output).unwrap())
-                );
-                // todo: debug
-                if hour > 0 && hour % 6 == 0 {
-                    println!(
-                        "solution: {:?}",
-                        hex::encode(node_to_bytes(&ctx.allocator, solution).unwrap())
+                let output = XchandlesPricingOutput::from_clvm(&ctx.allocator, output)?;
+
+                assert_eq!(output.registered_time, 366 * 24 * 60 * 60);
+
+                if hour == 0 {
+                    let scale_factor =
+                        372529029846191406_u128 * 1000_u128 / 1_000_000_000_000_000_000_u128;
+                    assert_eq!(
+                        output.price,
+                        (100_000_000 * 1000) / (1 << day) - scale_factor
                     );
                 }
+
+                assert!(output.price < last_price);
+                last_price = output.price;
             }
         }
+
+        // check premium after auction is 0
+        let solution = XchandlesExponentialPremiumRenewPuzzleSolution::<u64> {
+            handle: "yakuhito".to_string(),
+            expiration: 0,
+            buy_time: 28 * 24 * 60 * 60,
+            pricing_program_solution: 1,
+        }
+        .to_clvm(&mut ctx.allocator)?;
+
+        let output = ctx.run(puzzle, solution)?;
+        let output = XchandlesPricingOutput::from_clvm(&ctx.allocator, output)?;
+
+        assert_eq!(output.registered_time, 366 * 24 * 60 * 60);
+        assert_eq!(output.price, 0);
 
         Ok(())
     }
