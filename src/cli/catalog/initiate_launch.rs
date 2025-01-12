48b9cc07685951e424e7b7ed3bad92477127d735
Yak <y@kuhi.to>
2025-01-12 11:20:16 -0300

    adding cat maker to catalog

--- src/cli/catalog/initiate_launch.rs
+++ src/cli/catalog/initiate_launch.rs
@@ -1,94 +1,96 @@
-use crate::{
-    cli::{
-        chia_client::ChiaRpcClient,
-        csv::load_catalog_premine_csv,
-        prompt_for_value,
-        utils::{yes_no_prompt, CliError},
-        Db, CATALOG_LAUNCH_CATS_PER_SPEND_KEY, CATALOG_LAUNCH_GENERATION_KEY,
-        CATALOG_LAUNCH_LAUNCHER_ID_KEY,
-    },
-    CatalogRegistryConstants,
-};
-use chia_wallet_sdk::{
-    encode_address, Offer, SpendContext, MAINNET_CONSTANTS, TESTNET11_CONSTANTS,
-};
+// use crate::{
+//     cli::{
+//         chia_client::ChiaRpcClient,
+//         csv::load_catalog_premine_csv,
+//         prompt_for_value,
+//         utils::{yes_no_prompt, CliError},
+//         Db, CATALOG_LAUNCH_CATS_PER_SPEND_KEY, CATALOG_LAUNCH_GENERATION_KEY,
+//         CATALOG_LAUNCH_LAUNCHER_ID_KEY,
+//     },
+//     CatalogRegistryConstants,
+// };
+// use chia_wallet_sdk::{
+//     encode_address, Offer, SpendContext, MAINNET_CONSTANTS, TESTNET11_CONSTANTS,
+// };
 
-pub async fn catalog_initiate_launch(testnet11: bool) -> Result<(), CliError> {
-    println!("Welcome to the CATalog launch setup, deployer.");
+use crate::CliError;
 
-    println!("Opening database...");
-    let db = Db::new().await?;
+pub async fn catalog_initiate_launch(_testnet11: bool) -> Result<(), CliError> {
+    // println!("Welcome to the CATalog launch setup, deployer.");
 
-    println!("Loading premine data...");
+    // println!("Opening database...");
+    // let db = Db::new().await?;
 
-    let csv_filename = if testnet11 {
-        "catalog_premine_testnet11.csv"
-    } else {
-        "catalog_premine_mainnet.csv"
-    };
-    let cats_to_launch = load_catalog_premine_csv(csv_filename)?;
-    println!(
-        "Loaded {} CATs to be premined. First few records:",
-        cats_to_launch.len()
-    );
-    for record in cats_to_launch.iter().take(7) {
-        println!("  code: {:?}, name: {:?}", record.code, record.name);
-    }
+    // println!("Loading premine data...");
 
-    yes_no_prompt(
-        format!(
-            "Premine data was be loaded from '{}' - is this the correct data?",
-            csv_filename
-        )
-        .as_str(),
-    )?;
+    // let csv_filename = if testnet11 {
+    //     "catalog_premine_testnet11.csv"
+    // } else {
+    //     "catalog_premine_mainnet.csv"
+    // };
+    // let cats_to_launch = load_catalog_premine_csv(csv_filename)?;
+    // println!(
+    //     "Loaded {} CATs to be premined. First few records:",
+    //     cats_to_launch.len()
+    // );
+    // for record in cats_to_launch.iter().take(7) {
+    //     println!("  code: {:?}, name: {:?}", record.code, record.name);
+    // }
 
-    println!("Initializing Chia RPC client...");
-    let client = ChiaRpcClient::coinset(testnet11);
+    // yes_no_prompt(
+    //     format!(
+    //         "Premine data was be loaded from '{}' - is this the correct data?",
+    //         csv_filename
+    //     )
+    //     .as_str(),
+    // )?;
 
-    let launcher_id = db.get_value_by_key(CATALOG_LAUNCH_LAUNCHER_ID_KEY).await?;
-    if launcher_id.is_some() {
-        yes_no_prompt("Previous deployment found in db - do you wish to override?")?;
+    // println!("Initializing Chia RPC client...");
+    // let client = ChiaRpcClient::coinset(testnet11);
 
-        db.remove_key(CATALOG_LAUNCH_LAUNCHER_ID_KEY).await?;
-        let cats_per_spend = db
-            .get_value_by_key(CATALOG_LAUNCH_CATS_PER_SPEND_KEY)
-            .await?;
-        if cats_per_spend.is_some() {
-            db.remove_key(CATALOG_LAUNCH_CATS_PER_SPEND_KEY).await?;
-        }
+    // let launcher_id = db.get_value_by_key(CATALOG_LAUNCH_LAUNCHER_ID_KEY).await?;
+    // if launcher_id.is_some() {
+    //     yes_no_prompt("Previous deployment found in db - do you wish to override?")?;
 
-        let generation = db.get_value_by_key(CATALOG_LAUNCH_GENERATION_KEY).await?;
-        if generation.is_some() {
-            db.remove_key(CATALOG_LAUNCH_GENERATION_KEY).await?;
-        }
-    }
+    //     db.remove_key(CATALOG_LAUNCH_LAUNCHER_ID_KEY).await?;
+    //     let cats_per_spend = db
+    //         .get_value_by_key(CATALOG_LAUNCH_CATS_PER_SPEND_KEY)
+    //         .await?;
+    //     if cats_per_spend.is_some() {
+    //         db.remove_key(CATALOG_LAUNCH_CATS_PER_SPEND_KEY).await?;
+    //     }
 
-    let cats_per_unroll_str =
-        prompt_for_value("How many CATs should be deployed per unroll spend?")?;
-    let cats_per_unroll: u64 = cats_per_unroll_str.parse().map_err(CliError::ParseInt)?;
+    //     let generation = db.get_value_by_key(CATALOG_LAUNCH_GENERATION_KEY).await?;
+    //     if generation.is_some() {
+    //         db.remove_key(CATALOG_LAUNCH_GENERATION_KEY).await?;
+    //     }
+    // }
 
-    let constants = CatalogRegistryConstants::get(testnet11);
-    let prefix = if testnet11 { "txch" } else { "xch" };
-    let royalty_address =
-        encode_address(constants.royalty_address.into(), prefix).map_err(CliError::Bech32)?;
-    let precommit_payout_address =
-        encode_address(constants.precommit_payout_puzzle_hash.into(), prefix)
-            .map_err(CliError::Bech32)?;
+    // let cats_per_unroll_str =
+    //     prompt_for_value("How many CATs should be deployed per unroll spend?")?;
+    // let cats_per_unroll: u64 = cats_per_unroll_str.parse().map_err(CliError::ParseInt)?;
 
-    println!("Default constants will be used:");
-    println!("  royalty address: {}", royalty_address);
-    println!(
-        "  royalty ten thousandths: {}",
-        constants.royalty_ten_thousandths
-    );
-    println!("  precommit payout address: {}", precommit_payout_address);
-    println!(
-        "  relative block height: {}",
-        constants.relative_block_height
-    );
-    println!("  price singleton id: (will be launched as well)");
-    yes_no_prompt("Do the constants above have the correct values?")?;
+    // let constants = CatalogRegistryConstants::get(testnet11);
+    // let prefix = if testnet11 { "txch" } else { "xch" };
+    // let royalty_address =
+    //     encode_address(constants.royalty_address.into(), prefix).map_err(CliError::Bech32)?;
+    // let precommit_payout_address =
+    //     encode_address(constants.precommit_payout_puzzle_hash.into(), prefix)
+    //         .map_err(CliError::Bech32)?;
+
+    // println!("Default constants will be used:");
+    // println!("  royalty address: {}", royalty_address);
+    // println!(
+    //     "  royalty ten thousandths: {}",
+    //     constants.royalty_ten_thousandths
+    // );
+    // println!("  precommit payout address: {}", precommit_payout_address);
+    // println!(
+    //     "  relative block height: {}",
+    //     constants.relative_block_height
+    // );
+    // println!("  price singleton id: (will be launched as well)");
+    // yes_no_prompt("Do the constants above have the correct values?")?;
 
     // let price_schedule = price_schedule_for_catalog(testnet11);
 
