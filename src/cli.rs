ef183e35b82accfb35be2de9d8e1a8ca74401887
Yak <y@kuhi.to>
2025-01-22 18:45:29 -0300

    rm chia_rpc_client now that wallet-sdk is out

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,11 @@
 mod catalog;
-mod chia_client;
 mod commands;
-mod csv;
+// mod csv;
 mod database;
 mod utils;
 
 pub use catalog::*;
-pub use chia_client::*;
 pub use commands::*;
-pub use csv::*;
+// pub use csv::*;
 pub use database::*;
 pub use utils::*;
--- src/cli/chia_client.rs
+++
@@ -1,9 +0,0 @@
-pub mod client;
-pub mod de;
-pub mod mock_client;
-pub mod models;
-
-pub use client::*;
-pub use de::*;
-pub use mock_client::*;
-pub use models::*;
