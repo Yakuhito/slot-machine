fd415538d912f56e8ebdd9e1158386bb4202982a
Yak <y@kuhi.to>
2025-03-19 10:09:48 -0300

    checkpoint 2

--- src/cli/catalog/sync.rs
+++ src/cli/catalog/sync.rs
@@ -17,70 +17,78 @@ pub async fn sync_catalog(
 ) -> Result<CatalogRegistry, CliError> {
     let last_unspent_coin_id_str = db.get_value_by_key(CATALOG_LAST_UNSPENT_COIN).await?;
 
-    let mut last_spent_coin_id: Bytes32 =
-        if let Some(last_unspent_coin_id_str) = last_unspent_coin_id_str {
-            let last_unspent_coin_id = Bytes32::new(
-                hex::decode(last_unspent_coin_id_str)?
-                    .to_vec()
-                    .try_into()
-                    .unwrap(),
-            );
+    let mut last_coin_id: Bytes32 = if let Some(last_unspent_coin_id_str) = last_unspent_coin_id_str
+    {
+        let last_unspent_coin_id = Bytes32::new(
+            hex::decode(last_unspent_coin_id_str)?
+                .to_vec()
+                .try_into()
+                .unwrap(),
+        );
 
-            let coin_record_response = client.get_coin_record_by_name(last_unspent_coin_id).await?;
-            if let Some(coin_record) = coin_record_response.coin_record {
-                coin_record.coin.parent_coin_info
-            } else {
-                return Err(CliError::Driver(DriverError::Custom(
-                    "Could not find latest unspent coin".to_string(),
-                )));
-            }
+        let coin_record_response = client.get_coin_record_by_name(last_unspent_coin_id).await?;
+        if let Some(coin_record) = coin_record_response.coin_record {
+            coin_record.coin.parent_coin_info
         } else {
-            launcher_id
+            return Err(CliError::Driver(DriverError::Custom(
+                "Could not find latest unspent coin".to_string(),
+            )));
+        }
+    } else {
+        launcher_id
+    };
+
+    let mut catalog: Option<CatalogRegistry> = None;
+    loop {
+        let coin_record_response = client.get_coin_record_by_name(last_coin_id).await?;
+        let Some(coin_record) = coin_record_response.coin_record else {
+            break;
+        };
+        if !coin_record.spent {
+            return Err(CliError::Driver(DriverError::Custom(
+                "CATalog coin that should be spent is unspent".to_string(),
+            )));
+        }
+
+        let puzzle_and_solution_resp = client
+            .get_puzzle_and_solution(
+                coin_record.coin.coin_id(),
+                Some(coin_record.confirmed_block_index),
+            )
+            .await?;
+        let coin_spend = puzzle_and_solution_resp
+            .coin_solution
+            .ok_or(CliError::Driver(DriverError::Custom(
+                "Could not find puzzle and solution for a coin that should be spent".to_string(),
+            )))?;
+
+        let puzzle_ptr = ctx.alloc(&coin_spend.puzzle_reveal)?;
+        let parent_puzzle = Puzzle::parse(&ctx.allocator, puzzle_ptr);
+        let solution_ptr = ctx.alloc(&coin_spend.solution)?;
+        if let Some(prev_catalog) = catalog {
+            let new_slots = prev_catalog.get_new_slots_from_spend(ctx, solution_ptr);
+
+            todo!("yak");
+        }
+
+        let Some(some_catalog) = CatalogRegistry::from_parent_spend(
+            &mut ctx.allocator,
+            coin_record.coin,
+            parent_puzzle,
+            solution_ptr,
+            constants,
+        )?
+        else {
+            break;
         };
 
-    let mut slots = db
-        .get_slots::<CatalogSlotValue>(&mut ctx.allocator, launcher_id, 0)
-        .await?;
-
-    let mut coin_record_response = client.get_coin_record_by_name(last_spent_coin_id).await?;
-    let mut coin_record =
-        coin_record_response
-            .coin_record
-            .ok_or(CliError::Driver(DriverError::Custom(
-                "Could not find latest spent coin".to_string(),
-            )))?;
-    if !coin_record.spent {
-        return Err(CliError::Driver(DriverError::Custom(
-            "CATalog coin that should be spent is unspent".to_string(),
-        )));
+        last_coin_id = some_catalog.coin.coin_id();
+        catalog = Some(some_catalog);
     }
-
-    let mut puzzle_and_solution_resp = client
-        .get_puzzle_and_solution(
-            coin_record.coin.coin_id(),
-            Some(coin_record.confirmed_block_index),
-        )
-        .await?;
-    let mut coin_spend = puzzle_and_solution_resp
-        .coin_solution
-        .ok_or(CliError::Driver(DriverError::Custom(
-            "Could not find puzzle and solution for a coin that should be spent".to_string(),
-        )))?;
-
-    let puzzle_ptr = ctx.alloc(&coin_spend.puzzle_reveal)?;
-    let parent_puzzle = Puzzle::parse(&ctx.allocator, puzzle_ptr);
-    let solution_ptr = ctx.alloc(&coin_spend.solution)?;
-    let mut catalog = CatalogRegistry::from_parent_spend(
-        &mut ctx.allocator,
-        coin_record.coin,
-        parent_puzzle,
-        solution_ptr,
-        constants,
-    )?;
 
     db.save_key_value(
         CATALOG_LAST_UNSPENT_COIN,
-        &hex::encode(catalog.coin.coin_id().to_vec()),
+        &hex::encode(last_coin_id.to_vec()),
     )
     .await?;
     todo!("todo")
