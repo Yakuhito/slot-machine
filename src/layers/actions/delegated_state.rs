f713755ec0aee9e769e518b1baca7a76ae0fa5ae
Yak <y@kuhi.to>
2025-04-07 09:42:48 -0300

    delegated state

--- src/layers/actions/delegated_state.rs
+++ src/layers/actions/delegated_state.rs
@@ -3,6 +3,7 @@ use chia::{
     protocol::{Bytes32, Coin},
     puzzles::singleton::SingletonStruct,
 };
+use chia_puzzles::SINGLETON_TOP_LAYER_V1_1_HASH;
 use chia_wallet_sdk::{
     driver::{DriverError, Spend, SpendContext},
     types::Conditions,
@@ -83,24 +84,26 @@ impl DelegatedStateAction {
     }
 }
 
-pub const DELEGATED_STATE_ACTION_PUZZLE: [u8; 399] = hex!("ff02ffff01ff04ff27ffff04ffff04ff08ffff04ffff0112ffff04ffff02ff0effff04ff02ffff04ff27ff80808080ffff04ffff0bff2affff0bff0cffff0bff0cff32ff0980ffff0bff0cffff0bff3affff0bff0cffff0bff0cff32ffff02ff0effff04ff02ffff04ff05ff8080808080ffff0bff0cffff0bff3affff0bff0cffff0bff0cff32ff3780ffff0bff0cff32ff22808080ff22808080ff22808080ff8080808080ff808080ffff04ffff01ffff4302ffffffa04bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c385a5d7cce23c7785459aa09dcf97a184f32623d11a73124ceb99a5709b083721e878a16d78f596718ba7b2ffa102a12871fee210fb8619291eaea194581cbd2531e4b23759d225f6806923f63222a102a8d5dd63fba471ebcb1f3e8f7c1e1879b7152a6e7298a91ce119a63400ade7c5ff02ffff03ffff07ff0580ffff01ff0bffff0102ffff02ff0effff04ff02ffff04ff09ff80808080ffff02ff0effff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ff018080");
+pub const DELEGATED_STATE_ACTION_PUZZLE: [u8; 396] = hex!("ff02ffff01ff04ff819fffff04ff2fffff04ffff04ffff04ff08ffff04ffff0112ffff04ffff02ff0effff04ff02ffff04ff819fff80808080ffff04ffff0bff2affff0bff0cffff0bff0cff32ff0580ffff0bff0cffff0bff3affff0bff0cffff0bff0cff32ff0b80ffff0bff0cffff0bff3affff0bff0cffff0bff0cff32ff81df80ffff0bff0cff32ff22808080ff22808080ff22808080ff8080808080ff8080ff80808080ffff04ffff01ffff4302ffffffa04bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c385a5d7cce23c7785459aa09dcf97a184f32623d11a73124ceb99a5709b083721e878a16d78f596718ba7b2ffa102a12871fee210fb8619291eaea194581cbd2531e4b23759d225f6806923f63222a102a8d5dd63fba471ebcb1f3e8f7c1e1879b7152a6e7298a91ce119a63400ade7c5ff02ffff03ffff07ff0580ffff01ff0bffff0102ffff02ff0effff04ff02ffff04ff09ff80808080ffff02ff0effff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ff018080");
 
 pub const DELEGATED_STATE_ACTION_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    61d91188f5464625ec0224d47e0c18c2aa13c6f7e0d6ecbd2d928c64e068dfba
+    78726a59a2c2498e617d01d7fdbea88d2fccae39e267cd2f16ffcebbc769681c
     "
 ));
 
 #[derive(ToClvm, FromClvm, Debug, Clone, Copy, PartialEq, Eq)]
 #[clvm(curry)]
 pub struct DelegatedStateActionArgs {
-    pub other_singleton_struct: SingletonStruct,
+    pub singleton_mod_hash: Bytes32,
+    pub other_singleton_struct_hash: Bytes32,
 }
 
 impl DelegatedStateActionArgs {
     pub fn new(other_launcher_id: Bytes32) -> Self {
         Self {
-            other_singleton_struct: SingletonStruct::new(other_launcher_id),
+            singleton_mod_hash: SINGLETON_TOP_LAYER_V1_1_HASH.into(),
+            other_singleton_struct_hash: SingletonStruct::new(other_launcher_id).tree_hash().into(),
         }
     }
 }
