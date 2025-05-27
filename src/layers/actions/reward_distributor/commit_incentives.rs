2fefb77c3b950298e12ee760fd23e335cc262ba5
Yak <y@kuhi.to>
2025-05-27 20:01:39 +0300

    refactor commit incentive

--- src/layers/actions/reward_distributor/commit_incentives.rs
+++ src/layers/actions/reward_distributor/commit_incentives.rs
@@ -11,24 +11,27 @@ use clvmr::NodePtr;
 use hex_literal::hex;
 
 use crate::{
-    Action, DigCommitmentSlotValue, DigRewardDistributor, DigRewardDistributorConstants,
-    DigRewardSlotValue, DigSlotNonce, Slot, SpendContextExt,
+    Action, RewardDistributor, RewardDistributorCommitmentSlotValue, RewardDistributorConstants,
+    RewardDistributorRewardSlotValue, RewardDistributorSlotNonce, Slot, SpendContextExt,
 };
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct DigCommitIncentivesAction {
+pub struct RewardDistributorCommitIncentivesAction {
     pub launcher_id: Bytes32,
     pub epoch_seconds: u64,
 }
 
-impl ToTreeHash for DigCommitIncentivesAction {
+impl ToTreeHash for RewardDistributorCommitIncentivesAction {
     fn tree_hash(&self) -> TreeHash {
-        DigCommitIncentivesActionArgs::curry_tree_hash(self.launcher_id, self.epoch_seconds)
+        RewardDistributorCommitIncentivesActionArgs::curry_tree_hash(
+            self.launcher_id,
+            self.epoch_seconds,
+        )
     }
 }
 
-impl Action<DigRewardDistributor> for DigCommitIncentivesAction {
-    fn from_constants(constants: &DigRewardDistributorConstants) -> Self {
+impl Action<RewardDistributor> for RewardDistributorCommitIncentivesAction {
+    fn from_constants(constants: &RewardDistributorConstants) -> Self {
         Self {
             launcher_id: constants.launcher_id,
             epoch_seconds: constants.epoch_seconds,
@@ -36,11 +39,14 @@ impl Action<DigRewardDistributor> for DigCommitIncentivesAction {
     }
 }
 
-impl DigCommitIncentivesAction {
+impl RewardDistributorCommitIncentivesAction {
     fn construct_puzzle(&self, ctx: &mut SpendContext) -> Result<NodePtr, DriverError> {
         CurriedProgram {
-            program: ctx.dig_commit_incentives_action_puzzle()?,
-            args: DigCommitIncentivesActionArgs::new(self.launcher_id, self.epoch_seconds),
+            program: ctx.reward_distributor_commit_incentives_action_puzzle()?,
+            args: RewardDistributorCommitIncentivesActionArgs::new(
+                self.launcher_id,
+                self.epoch_seconds,
+            ),
         }
         .to_clvm(ctx)
         .map_err(DriverError::ToClvm)
@@ -54,35 +60,35 @@ impl DigCommitIncentivesAction {
         solution: NodePtr,
     ) -> Result<
         (
-            DigCommitmentSlotValue,
-            Vec<DigRewardSlotValue>,
-            (DigSlotNonce, Bytes32), // spent slot
+            RewardDistributorCommitmentSlotValue,
+            Vec<RewardDistributorRewardSlotValue>,
+            (RewardDistributorSlotNonce, Bytes32), // spent slot
         ),
         DriverError,
     > {
-        let solution = ctx.extract::<DigCommitIncentivesActionSolution>(solution)?;
+        let solution = ctx.extract::<RewardDistributorCommitIncentivesActionSolution>(solution)?;
 
-        let commitment_slot_value = DigCommitmentSlotValue {
+        let commitment_slot_value = RewardDistributorCommitmentSlotValue {
             epoch_start: solution.epoch_start,
             clawback_ph: solution.clawback_ph,
             rewards: solution.rewards_to_add,
         };
 
-        let mut reward_slot_values: Vec<DigRewardSlotValue> = vec![];
+        let mut reward_slot_values: Vec<RewardDistributorRewardSlotValue> = vec![];
 
         if solution.slot_epoch_time == solution.epoch_start {
-            reward_slot_values.push(DigRewardSlotValue {
+            reward_slot_values.push(RewardDistributorRewardSlotValue {
                 epoch_start: solution.epoch_start,
                 next_epoch_initialized: solution.slot_next_epoch_initialized,
                 rewards: solution.slot_total_rewards + solution.rewards_to_add,
             })
         } else {
-            reward_slot_values.push(DigRewardSlotValue {
+            reward_slot_values.push(RewardDistributorRewardSlotValue {
                 epoch_start: solution.slot_epoch_time,
                 next_epoch_initialized: true,
                 rewards: solution.slot_total_rewards,
             });
-            reward_slot_values.push(DigRewardSlotValue {
+            reward_slot_values.push(RewardDistributorRewardSlotValue {
                 epoch_start: solution.epoch_start,
                 next_epoch_initialized: false,
                 rewards: solution.rewards_to_add,
@@ -91,7 +97,7 @@ impl DigCommitIncentivesAction {
             let mut start_epoch_time = solution.slot_epoch_time + epoch_seconds;
             let end_epoch_time = solution.epoch_start;
             while end_epoch_time > start_epoch_time {
-                reward_slot_values.push(DigRewardSlotValue {
+                reward_slot_values.push(RewardDistributorRewardSlotValue {
                     epoch_start: start_epoch_time,
                     next_epoch_initialized: true,
                     rewards: 0,
@@ -102,8 +108,8 @@ impl DigCommitIncentivesAction {
         }
 
         let spent_slot = (
-            DigSlotNonce::REWARD,
-            DigRewardSlotValue {
+            RewardDistributorSlotNonce::REWARD,
+            RewardDistributorRewardSlotValue {
                 epoch_start: solution.slot_epoch_time,
                 next_epoch_initialized: solution.slot_next_epoch_initialized,
                 rewards: solution.slot_total_rewards,
@@ -119,20 +125,20 @@ impl DigCommitIncentivesAction {
     pub fn spend(
         self,
         ctx: &mut SpendContext,
-        distributor: &mut DigRewardDistributor,
-        reward_slot: Slot<DigRewardSlotValue>,
+        distributor: &mut RewardDistributor,
+        reward_slot: Slot<RewardDistributorRewardSlotValue>,
         epoch_start: u64,
         clawback_ph: Bytes32,
         rewards_to_add: u64,
     ) -> Result<
         (
             Conditions,
-            Slot<DigCommitmentSlotValue>,
-            Vec<Slot<DigRewardSlotValue>>,
+            Slot<RewardDistributorCommitmentSlotValue>,
+            Vec<Slot<RewardDistributorRewardSlotValue>>,
         ),
         DriverError,
     > {
-        let new_commitment_slot_value = DigCommitmentSlotValue {
+        let new_commitment_slot_value = RewardDistributorCommitmentSlotValue {
             epoch_start,
             clawback_ph,
             rewards: rewards_to_add,
@@ -147,7 +153,7 @@ impl DigCommitIncentivesAction {
         reward_slot.spend(ctx, distributor.info.inner_puzzle_hash().into())?;
 
         // spend self
-        let action_solution = ctx.alloc(&DigCommitIncentivesActionSolution {
+        let action_solution = ctx.alloc(&RewardDistributorCommitIncentivesActionSolution {
             slot_epoch_time: reward_slot.info.value.epoch_start,
             slot_next_epoch_initialized: reward_slot.info.value.next_epoch_initialized,
             slot_total_rewards: reward_slot.info.value.rewards,
@@ -171,16 +177,19 @@ impl DigCommitIncentivesAction {
             )),
             distributor.created_slot_values_to_slots(
                 vec![new_commitment_slot_value],
-                DigSlotNonce::COMMITMENT,
+                RewardDistributorSlotNonce::COMMITMENT,
             )[0],
-            distributor.created_slot_values_to_slots(reward_slot_values, DigSlotNonce::REWARD),
+            distributor.created_slot_values_to_slots(
+                reward_slot_values,
+                RewardDistributorSlotNonce::REWARD,
+            ),
         ))
     }
 }
 
-pub const DIG_COMMIT_INCENTIVES_PUZZLE: [u8; 1209] = hex!("ff02ffff01ff02ffff03ffff22ffff20ffff15ff820defff8205df8080ffff15ff820fdfff808080ffff01ff04ffff04ff80ffff04ffff10ff81afff820fdf80ffff04ff82016fffff04ff8202efffff04ff8205efff808080808080ffff02ff12ffff04ff02ffff04ff0bffff04ffff0bffff0102ffff0bffff0101ff8205df80ffff0bffff0102ffff0bffff0101ff820bdf80ffff0bffff0101ff820fdf808080ffff04ff820bdfffff04ffff04ffff02ff3effff04ff02ffff04ff05ffff04ffff02ff16ffff04ff02ffff04ff819fffff04ff82015fffff04ff8202dfff808080808080ff8080808080ffff02ffff03ffff09ff8205dfff819f80ffff01ff04ffff02ff1affff04ff02ffff04ff05ffff04ffff02ff16ffff04ff02ffff04ff819fffff04ff82015fffff04ffff10ff8202dfff820fdf80ff808080808080ffff04ffff0bffff0101ff819f80ff808080808080ff8080ffff01ff02ffff03ff82015fffff01ff0880ffff01ff04ffff02ff1affff04ff02ffff04ff05ffff04ffff02ff16ffff04ff02ffff04ff819fffff04ffff0101ffff04ff8202dfff808080808080ffff04ffff0bffff0101ff819f80ff808080808080ffff04ffff02ff1affff04ff02ffff04ff05ffff04ffff02ff16ffff04ff02ffff04ff8205dfffff04ff80ffff04ff820fdfff808080808080ffff04ffff0bffff0101ff8205df80ff808080808080ffff02ff2effff04ff02ffff04ff05ffff04ff17ffff04ffff10ff819fff1780ffff04ff8205dfff80808080808080808080ff018080ff018080ff8080808080808080ffff01ff088080ff0180ffff04ffff01ffffff333eff42ff02ffffa04bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c385a5d7cce23c7785459aa09dcf97a184f32623d11a73124ceb99a5709b083721e878a16d78f596718ba7b2ffa102a12871fee210fb8619291eaea194581cbd2531e4b23759d225f6806923f63222a102a8d5dd63fba471ebcb1f3e8f7c1e1879b7152a6e7298a91ce119a63400ade7c5ffffff04ffff04ff18ffff04ffff0effff0163ff0b80ff808080ffff04ffff02ff1affff04ff02ffff04ff05ffff04ff0bffff04ff17ff808080808080ff2f8080ff04ff10ffff04ffff0bff81bcffff0bff2cffff0bff2cff81dcff0580ffff0bff2cffff0bff81fcffff0bff2cffff0bff2cff81dcffff0bffff0101ff0b8080ffff0bff2cff81dcff819c808080ff819c808080ffff04ff80ffff04ffff04ff17ff8080ff8080808080ffff0bffff0102ffff0bffff0101ff0580ffff0bffff0102ffff0bffff0101ff0b80ffff0bffff0101ff17808080ffff02ffff03ffff09ff17ff2f80ff80ffff01ff04ffff02ff1affff04ff02ffff04ff05ffff04ffff02ff16ffff04ff02ffff04ff17ffff01ff01ff8080808080ffff04ffff0bffff0101ff1780ff808080808080ffff02ff2effff04ff02ffff04ff05ffff04ff0bffff04ffff10ff17ff0b80ffff04ff2fff808080808080808080ff0180ff04ff14ffff04ffff0112ffff04ff80ffff04ffff0bff81bcffff0bff2cffff0bff2cff81dcff0580ffff0bff2cffff0bff81fcffff0bff2cffff0bff2cff81dcffff0bffff0101ff0b8080ffff0bff2cff81dcff819c808080ff819c808080ff8080808080ff018080");
+pub const REWARD_DISTRIBUTOR_COMMIT_INCENTIVES_PUZZLE: [u8; 1209] = hex!("ff02ffff01ff02ffff03ffff22ffff20ffff15ff820defff8205df8080ffff15ff820fdfff808080ffff01ff04ffff04ff80ffff04ffff10ff81afff820fdf80ffff04ff82016fffff04ff8202efffff04ff8205efff808080808080ffff02ff12ffff04ff02ffff04ff0bffff04ffff0bffff0102ffff0bffff0101ff8205df80ffff0bffff0102ffff0bffff0101ff820bdf80ffff0bffff0101ff820fdf808080ffff04ff820bdfffff04ffff04ffff02ff3effff04ff02ffff04ff05ffff04ffff02ff16ffff04ff02ffff04ff819fffff04ff82015fffff04ff8202dfff808080808080ff8080808080ffff02ffff03ffff09ff8205dfff819f80ffff01ff04ffff02ff1affff04ff02ffff04ff05ffff04ffff02ff16ffff04ff02ffff04ff819fffff04ff82015fffff04ffff10ff8202dfff820fdf80ff808080808080ffff04ffff0bffff0101ff819f80ff808080808080ff8080ffff01ff02ffff03ff82015fffff01ff0880ffff01ff04ffff02ff1affff04ff02ffff04ff05ffff04ffff02ff16ffff04ff02ffff04ff819fffff04ffff0101ffff04ff8202dfff808080808080ffff04ffff0bffff0101ff819f80ff808080808080ffff04ffff02ff1affff04ff02ffff04ff05ffff04ffff02ff16ffff04ff02ffff04ff8205dfffff04ff80ffff04ff820fdfff808080808080ffff04ffff0bffff0101ff8205df80ff808080808080ffff02ff2effff04ff02ffff04ff05ffff04ff17ffff04ffff10ff819fff1780ffff04ff8205dfff80808080808080808080ff018080ff018080ff8080808080808080ffff01ff088080ff0180ffff04ffff01ffffff333eff42ff02ffffa04bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c385a5d7cce23c7785459aa09dcf97a184f32623d11a73124ceb99a5709b083721e878a16d78f596718ba7b2ffa102a12871fee210fb8619291eaea194581cbd2531e4b23759d225f6806923f63222a102a8d5dd63fba471ebcb1f3e8f7c1e1879b7152a6e7298a91ce119a63400ade7c5ffffff04ffff04ff18ffff04ffff0effff0163ff0b80ff808080ffff04ffff02ff1affff04ff02ffff04ff05ffff04ff0bffff04ff17ff808080808080ff2f8080ff04ff10ffff04ffff0bff81bcffff0bff2cffff0bff2cff81dcff0580ffff0bff2cffff0bff81fcffff0bff2cffff0bff2cff81dcffff0bffff0101ff0b8080ffff0bff2cff81dcff819c808080ff819c808080ffff04ff80ffff04ffff04ff17ff8080ff8080808080ffff0bffff0102ffff0bffff0101ff0580ffff0bffff0102ffff0bffff0101ff0b80ffff0bffff0101ff17808080ffff02ffff03ffff09ff17ff2f80ff80ffff01ff04ffff02ff1affff04ff02ffff04ff05ffff04ffff02ff16ffff04ff02ffff04ff17ffff01ff01ff8080808080ffff04ffff0bffff0101ff1780ff808080808080ffff02ff2effff04ff02ffff04ff05ffff04ff0bffff04ffff10ff17ff0b80ffff04ff2fff808080808080808080ff0180ff04ff14ffff04ffff0112ffff04ff80ffff04ffff0bff81bcffff0bff2cffff0bff2cff81dcff0580ffff0bff2cffff0bff81fcffff0bff2cffff0bff2cff81dcffff0bffff0101ff0b8080ffff0bff2cff81dcff819c808080ff819c808080ff8080808080ff018080");
 
-pub const DIG_COMMIT_INCENTIVES_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
+pub const REWARD_DISTRIBUTOR_COMMIT_INCENTIVES_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
     c30f51e746a212e5685654e01c5df5de43592792177d22326061a6c88ea2b72a
     "
@@ -188,23 +197,23 @@ pub const DIG_COMMIT_INCENTIVES_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
 
 #[derive(ToClvm, FromClvm, Debug, Clone, Copy, PartialEq, Eq)]
 #[clvm(curry)]
-pub struct DigCommitIncentivesActionArgs {
+pub struct RewardDistributorCommitIncentivesActionArgs {
     pub reward_slot_1st_curry_hash: Bytes32,
     pub commitment_slot_1st_curry_hash: Bytes32,
     pub epoch_seconds: u64,
 }
 
-impl DigCommitIncentivesActionArgs {
+impl RewardDistributorCommitIncentivesActionArgs {
     pub fn new(launcher_id: Bytes32, epoch_seconds: u64) -> Self {
         Self {
             reward_slot_1st_curry_hash: Slot::<()>::first_curry_hash(
                 launcher_id,
-                DigSlotNonce::REWARD.to_u64(),
+                RewardDistributorSlotNonce::REWARD.to_u64(),
             )
             .into(),
             commitment_slot_1st_curry_hash: Slot::<()>::first_curry_hash(
                 launcher_id,
-                DigSlotNonce::COMMITMENT.to_u64(),
+                RewardDistributorSlotNonce::COMMITMENT.to_u64(),
             )
             .into(),
             epoch_seconds,
@@ -212,11 +221,11 @@ impl DigCommitIncentivesActionArgs {
     }
 }
 
-impl DigCommitIncentivesActionArgs {
+impl RewardDistributorCommitIncentivesActionArgs {
     pub fn curry_tree_hash(launcher_id: Bytes32, epoch_seconds: u64) -> TreeHash {
         CurriedProgram {
-            program: DIG_COMMIT_INCENTIVES_PUZZLE_HASH,
-            args: DigCommitIncentivesActionArgs::new(launcher_id, epoch_seconds),
+            program: REWARD_DISTRIBUTOR_COMMIT_INCENTIVES_PUZZLE_HASH,
+            args: RewardDistributorCommitIncentivesActionArgs::new(launcher_id, epoch_seconds),
         }
         .tree_hash()
     }
@@ -224,7 +233,7 @@ impl DigCommitIncentivesActionArgs {
 
 #[derive(FromClvm, ToClvm, Debug, Clone, PartialEq, Eq)]
 #[clvm(solution)]
-pub struct DigCommitIncentivesActionSolution {
+pub struct RewardDistributorCommitIncentivesActionSolution {
     pub slot_epoch_time: u64,
     pub slot_next_epoch_initialized: bool,
     pub slot_total_rewards: u64,
