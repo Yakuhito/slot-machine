18d4977ab9b9215baff02727e173bbb58e7b14ef
Yak <y@kuhi.to>
2025-05-30 18:42:44 +0300

    aaaaaaaah

--- src/cli/xchandles.rs
+++ src/cli/xchandles.rs
@@ -1,11 +1,13 @@
 mod continue_launch;
 mod initiate_launch;
+mod register;
 mod sync;
 mod unroll_state_scheduler;
 mod verify_deployment;
 
 pub use continue_launch::*;
 pub use initiate_launch::*;
+pub use register::*;
 pub use sync::*;
 pub use unroll_state_scheduler::*;
 pub use verify_deployment::*;
