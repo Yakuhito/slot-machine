b85f8403486ec5ee99afd8938227e85113b02c17
Yak <y@kuhi.to>
2025-06-25 11:20:13 +0300

    quick sync w/ mempool support

--- src/cli/catalog/quick_sync.rs
+++ src/cli/catalog/quick_sync.rs
@@ -42,19 +42,49 @@ pub async fn quick_sync_catalog(
 
         let mut temp_ctx = SpendContext::new();
         let catalog_maybe =
-            CatalogRegistry::from_parent_spend(&mut temp_ctx, &next_spend, constants)
-                .unwrap_or_default();
+            CatalogRegistry::from_parent_spend(&mut temp_ctx, &next_spend, constants)?;
         if catalog_maybe.is_some() {
             coin_spend = Some(next_spend);
             break;
         }
     }
 
-    if let Some(coin_spend) = coin_spend {
-        CatalogRegistry::from_parent_spend(ctx, &coin_spend, constants)?.ok_or(CliError::Custom(
-            "Tried to unwrap CATalog but couldn't".to_string(),
-        ))
-    } else {
-        Err(CliError::Custom("Could not find CATalog coin".to_string()))
+    let Some(coin_spend) = coin_spend else {
+        return Err(CliError::Custom("Could not find CATalog coin".to_string()));
+    };
+    let on_chain_catalog = CatalogRegistry::from_parent_spend(ctx, &coin_spend, constants)?.ok_or(
+        CliError::Custom("Could not parse CATalog spend".to_string()),
+    )?;
+
+    let Some(mut mempool_items) = client
+        .get_mempool_items_by_coin_name(on_chain_catalog.coin.coin_id())
+        .await?
+        .mempool_items
+    else {
+        return Ok(on_chain_catalog);
+    };
+
+    if mempool_items.len() == 0 {
+        return Ok(on_chain_catalog);
     }
+
+    let mempool_item = mempool_items.remove(0);
+    let mut catalog = on_chain_catalog;
+    loop {
+        let Some(catalog_spend) = mempool_item
+            .spend_bundle
+            .coin_spends
+            .iter()
+            .find(|c| c.coin.coin_id() == catalog.coin.coin_id())
+        else {
+            break;
+        };
+
+        let Some(new_catalog) = CatalogRegistry::from_spend(ctx, catalog_spend, constants)? else {
+            break;
+        };
+        catalog = new_catalog;
+    }
+
+    Ok(catalog)
 }
