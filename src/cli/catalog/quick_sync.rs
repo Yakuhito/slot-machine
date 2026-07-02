16448e9aa737d652e12b5532d48175cb6eb63e51
Yak <y@kuhi.to>
2026-07-02 15:44:25 +0300

    remove mempool_catalog_maybe entirely :)

--- src/cli/catalog/quick_sync.rs
+++ src/cli/catalog/quick_sync.rs
@@ -4,7 +4,7 @@ use chia_wallet_sdk::{
     driver::{CatalogRegistry, CatalogRegistryConstants, SpendContext},
 };
 
-use crate::{mempool_catalog_maybe, CliError};
+use crate::CliError;
 
 pub async fn quick_sync_catalog(
     client: &CoinsetClient,
@@ -52,9 +52,26 @@ pub async fn quick_sync_catalog(
     let Some(coin_spend) = coin_spend else {
         return Err(CliError::Custom("Could not find CATalog coin".to_string()));
     };
+
+    if let Some(mempool_items) = client
+        .get_mempool_items_by_coin_name(coin_spend.coin.coin_id())
+        .await?
+        .mempool_items
+    {
+        if !mempool_items.is_empty() {
+            if let Some(new_catalog) = CatalogRegistry::from_mempool_item(
+                ctx,
+                mempool_items[0].spend_bundle.clone(),
+                constants,
+            )? {
+                return Ok(new_catalog);
+            }
+        }
+    }
+
     let on_chain_catalog = CatalogRegistry::from_parent_spend(ctx, &coin_spend, constants)?.ok_or(
         CliError::Custom("Could not parse CATalog spend".to_string()),
     )?;
 
-    mempool_catalog_maybe(ctx, on_chain_catalog, client).await
+    Ok(on_chain_catalog)
 }
